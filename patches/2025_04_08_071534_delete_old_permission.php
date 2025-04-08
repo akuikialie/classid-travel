59cdcf773d1d048a2f6ed46a8fddac3a8b3a9806
winatabayu <winatabayu01@gmail.com>
2025-04-08 17:44:51 +0700

    refactor: create patcher to delete old permission

---
+++ patches/2025_04_08_071534_delete_old_permission.php
@@ -0,0 +1,34 @@
+<?php
+
+use Dentro\Patcher\Patch;
+
+return new class extends Patch
+{
+    public function __construct()
+    {
+        $this->isPerpetual = true;
+    }
+
+    public function eligible()
+    {
+        return true;
+    }
+    /**
+     * Run patch script.
+     *
+     * @return void
+     */
+    public function patch(): void
+    {
+        $permissionGroups = [];
+        foreach (\App\Enums\Permissions\RegisterPermissions::cases() as $key => $registerPermissions){
+            $permissionGroups[] = [
+                'name' => $registerPermissions->value::getGroupName()
+            ];
+        }
+
+        \App\Models\Spatie\Permission::query()
+            ->whereNotIn('group', collect($permissionGroups)->pluck('name')->toArray())
+            ->forceDelete();
+    }
+};
