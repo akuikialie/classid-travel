e892ed1850d7ab8296c480e39ab26c24013bc2d6
winatabayu <winatabayu01@gmail.com>
2025-04-09 15:36:10 +0700

    fix: update media model

--- app/Models/Base/Media.php
+++ app/Models/Base/Media.php
@@ -2,32 +2,10 @@
 
 namespace App\Models\Base;
 
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Spatie\MediaLibrary\HasMedia;
-use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media as BaseMedia;
 
-class Media extends BaseMedia implements HasMedia
+class Media extends BaseMedia
 {
-    use HasFactory;
-    use InteractsWithMedia;
     use SoftDeletes;
-
-    protected $table = 'media';
-
-    /**
-     * The "type" of the primary key ID.
-     *
-     * @var string
-     */
-    protected $keyType = 'uuid';
-
-    /**
-     * Indicates if the IDs are auto-incrementing.
-     *
-     * @var bool
-     */
-    public $incrementing = false;
 }
