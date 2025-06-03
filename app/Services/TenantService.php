d572fb3fce22cd4002fba4d288176732550cfd72
winatabayu <winatabayu01@gmail.com>
2025-06-03 14:50:19 +0700

    fix: cannot change avatar tenant

--- app/Services/TenantService.php
+++ app/Services/TenantService.php
@@ -162,7 +162,7 @@ class TenantService
             $tenant->name = $input['name'];
             $tenant->slug = $input['slug'];
         }
-        $tenant->fee_admin = $input['fee_admin'];
+        $tenant->fee_admin = $input['fee_admin'] ?? $tenant->fee_admin;
         $tenant->save();
 
         return $tenant->fresh();
