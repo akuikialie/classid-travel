6873c36b8a4d52a19b780eaa9ade453cf2efa9c0
akuikialie <aku.4l13@gmail.com>
2025-06-03 13:18:53 +0700

    [admin] : fix code style on route

--- app/Http/Routes/Web/Admin/DefaultRoute.php
+++ app/Http/Routes/Web/Admin/DefaultRoute.php
@@ -7,12 +7,13 @@ use Dentro\Yalr\BaseRoute;
 
 class DefaultRoute extends BaseRoute
 {
-
     public function register(): void
     {
         $this->router->middleware(['auth:sanctum', 'verified'])->group(function () {
-            $this->router->get('/dashboard',
-                [DashboardController::class, 'index'])
+            $this->router->get(
+                '/dashboard',
+                [DashboardController::class, 'index']
+            )
                 ->name($this->name('dashboard'));
         });
     }
