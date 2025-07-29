67d7ebc238026f6b5a36bc39b7aab053deda84ea
akuikialie <aku.4l13@gmail.com>
2025-07-30 04:11:18 +0700

    [transaction][export] : trial add last row value column

--- app/Exports/TransactionExport.php
+++ app/Exports/TransactionExport.php
@@ -139,10 +139,21 @@ class TransactionExport extends DefaultValueBinder implements FromCollection, Wi
 
                 $lastRow = $event->sheet->getDelegate()->getHighestRow();
 
-                $sheet = $event->sheet;
-                $sheet->cell('E' . ($lastRow + 1))->setValue(5);
+                // Append the summary row
+                $event->sheet->getDelegate()->appendRow([
+                    '1',
+                    '2',
+                    '3',
+                    '4',
+                    '5',
+                    '6',
+                    '7',
+                    '8',
+                    '9',
+                    '10',
+                ]);
 
-                $event->sheet->getStyle('B' . ($lastRow + 1) . ':J' . ($lastRow + 1))->applyFromArray([
+                $event->sheet->getStyle('A' . ($lastRow + 1) . ':J' . ($lastRow + 1))->applyFromArray([
                     'font' => [
                         'bold' => true
                     ],
